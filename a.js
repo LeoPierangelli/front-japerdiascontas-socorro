69b1a0131dbd859e1dbb499ee7f9b06d36a3f2bc
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-10-11 09:06:00 -0300

    feat index and js

---
+++ a.js
@@ -0,0 +1,32 @@
+// window.onload = function carregarPagina() {
+//     alert("Hello")
+// }
+
+function logar(){
+    var botao = document.getElementById("btnLogar")
+    //redireciona o usuario
+
+    // window.location.href = "https://www.google.com.br/"
+    
+    //reload
+    // window.location.reload()
+    
+
+    //navega pelo historico
+    // window.history.back()
+    // window.history.forward()
+
+
+    // setTimeout(function mensagem() {
+    //     alert("compre agora!!!")
+    // }, 2000)
+
+
+
+    
+
+
+
+}
+
+
