ae1af902b15a33fe17837e19cbcf6ca9d4b0a62d
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-10-11 09:49:41 -0300

    uma penca de comandos

--- a.js
+++ a.js
@@ -1,32 +1,61 @@
-// window.onload = function carregarPagina() {
-//     alert("Hello")
+// // window.onload = function carregarPagina() {
+// //     alert("Hello")
+// // }
+
+// function logar(){
+//     //redireciona o usuario
+//     // window.location.href = "https://www.google.com.br/"
+    
+//     //reload
+//     // window.location.reload()
+    
+
+//     //navega pelo historico
+//     // window.history.back()
+//     // window.history.forward()
+
+//     //ação dps de x segundos
+//     // setTimeout(function mensagem() {
+//     //     alert("compre agora!!!")
+//     // }, 2000)
+
+//     //ação repetitiva
+//     // setInterval(function mensagem(){
+//     //     console.log("noura ce")
+//     // }, 800)
+
+
+//     var textop1 = document.querySelector("#textop1")
+//     // paragrafo.innerHTML = "pikachu"
+
+//     var cardapio = document.querySelector("#cardapio").value
+//     switch(cardapio) {
+//         case 'cafe':
+//             textop1.innerHTML = `
+//             <h1>Café da manhã</h1>
+//             <p>Pão com nutella</p>
+//             <a href="#"> Compre agora! </a>
+//             `
+//         break;
+//         case 'almoco':
+//             textop1.innerHTML = "McDonalds"
+//         break;
+//         case 'jantar':
+//             textop1.innerHTML = "Pizza"
+//         break;
+//         default:
+//             textop1.innerHTML = "Opção inválida"
+//     }
+
+
 // }
 
-function logar(){
-    var botao = document.getElementById("btnLogar")
-    //redireciona o usuario
+// function trocarImagem(){
+//     var botao = document.querySelector("#btnImagem")
+//     var imagem = document.querySelector("#imagem")
+//     imagem.src = "https://images.pexels.com/photos/638689/pexels-photo-638689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//     imagem.width = "400"
+//     botao.style.color = "red"
+//     botao.classList.toggle("active")
 
-    // window.location.href = "https://www.google.com.br/"
-    
-    //reload
-    // window.location.reload()
-    
-
-    //navega pelo historico
-    // window.history.back()
-    // window.history.forward()
-
-
-    // setTimeout(function mensagem() {
-    //     alert("compre agora!!!")
-    // }, 2000)
-
-
-
-    
-
-
-
-}
-
-
+// }
